5698dc40d29821168dfdaf481dd2eca83d05ec19
happyhunter53 <forever2mail@naver.com>
2025-02-24 16:39:45 +0900

    0224

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -1,1 +1,14 @@
-print("키움소스 깃 시작하기.")
+import random
+
+# 로또 번호 생성 함수
+def generate_lotto():
+    # 1부터 45까지 숫자 중 6개를 무작위로 선택
+    lotto_numbers = random.sample(range(1, 46), 6)
+    # 오름차순으로 정렬
+    lotto_numbers.sort()
+    return lotto_numbers
+
+# 메인 실행
+print("로또 번호 추첨 결과:")
+numbers = generate_lotto()
+print(numbers)
--- stock.py
+++ stock.py
@@ -1,15 +1,5 @@
-def cal_upper(price):
-    increment = price * 0.3
-    upper_price = price + increment
-    return upper_price
+import win32com.client
 
-def cal_lower(price):
-    decrement = price * 0.3
-    lower_price = price - decrement
-    return lower_price
-
-author = 'pystock'
-
-print(cal_upper(10000))
-print(cal_lower(10000))
+explore = win32com.clinetdispatch("InternetExplorer.Application")
+explore.Visible + True
 
